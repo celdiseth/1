80af16a9b632a8921e859da1401319c02e8f7edf
Cenek Sekavec <cenek.sekavec@gmail.com>
2025-11-05 13:03:09 -0600

    feck

--- start.py
+++ start.py
@@ -11,22 +11,6 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = main_dir
 
 
-class tile(pg.sprite.Sprite):
-    def __init__(self, image, x, y, id):
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.image.load(os.path.join(data_dir, "arraydemo.bmp"))
-        self.id = 0
-
-class window(pg.Surface):
-    def __init__(self, tiles):
-        pg.Surface.__init__(self)
-        self.tiles =[]
-        
-
-    def add_tile(self, tile):
-        window.tiles[tile.x,tile.y] = tile
-        self.
-
 
 ######Classes SUCK donkey dick -- why complicate this
 
@@ -70,7 +54,47 @@ def second():
     s = pg.image.load(os.path.join(data_dir, "arraydemo.bmp"))
     pg.display.set_mode((255, 255))
     surface = pg.Surface((255, 255))
-    show(s)
+    #show(s)
+
+
+    a_tile = pg.image.load(os.path.join(data_dir, "arraydemo.bmp"))
+    tile_rect = a_tile.get_rect
+    x_stride = a_tile.get_width
+    y_stride = a_tile.get_height
+    a_tile_row = [1,0]
+    another_tile_row = [0,1]
+    all_tiles = []   #representation of the tiles. each unique sprite gets a number assigned. 
+    all_tiles.append(a_tile_row)
+    all_tiles.append(another_tile_row)
+    print(all_tiles)
+
+    this_row = 0
+    this_col = 0
+    screen = pg.display.get_surface()
+    for row in all_tiles:
+        
+        for tile in row:
+            if tile == 1:
+                #sx = this_col * x_stride
+                #sy = this_row * y_stride
+                #screen.blit(a_tile, tile_rect)
+                sx=+1
+            this_col =+ 1
+        
+        this_col = 0
+        this_row =+ 1
+            
+    screen.blit(a_tile, (0,0))
+    screen.blit(a_tile,(77,77))
+
+    while True:
+        event = pg.event.wait()
+        if event.type == pg.QUIT:
+            pg.quit()
+            raise SystemExit
+        if event.type in [pg.MOUSEBUTTONDOWN, pg.KEYDOWN]:
+            break
+
 
 if __name__ == "__main__":
     #main()
